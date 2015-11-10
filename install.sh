adbaa9b1f4752715cc405bbb3bf847ba6a7110ce
Andrea Leopardi <an.leopardi@gmail.com>
2015-11-10 23:56:02 +0100

    Add a .gitignore file and perfect the install script

--- install.sh
+++ install.sh
@@ -2,18 +2,31 @@
 
 set -e
 
-ROOT="$DOTFILES/vim"
+ROOT="$DOTVIM"
+
+symlink_files() {
+  echo "Symlinking *.symlink files"
+  for file in $(find $DOTVIM -name '*.symlink'); do
+    local _symlinked=".$(basename ${file%.symlink})"
+    ln -sfv "$file" "$HOME/${_symlinked}"
+  done
+}
 
 # Backup the ~/.vim directory and link the new one.
-mv "$HOME/.vim" "$HOME/.vim-whatyouhide-bkp"
-ln -sv "$ROOT" "$HOME/.vim"
-
-if [[ ! -e "$HOME/.colors.vim" ]]; then
-  cp -v "$ROOT/colors.vim.example" "$HOME/.colors.vim"
+if [[ -d "$HOME/.vim" ]]; then
+  if [[ -d "$HOME/.vim-whatyouhide-bkp" ]]; then
+    rm -rfv "$HOME/.vim"
+  else
+    mv "$HOME/.vim" "$HOME/.vim-whatyouhide-bkp"
+  fi
 fi
 
-# Temporary plugins.
-touch "$ROOT/tmp-plugins.vim"
+ln -sfv "$ROOT" "$HOME/.vim"
+
+if [[ ! -e "$HOME/.colors.vim" ]]; then
+  echo "Creating ~/.colors.vim for spot-on colors setup in vim"
+  cp -v "$ROOT/colors.vim.example" "$HOME/.colors.vim"
+fi
 
 # vim-plug
 _autoload="$ROOT/autoload"
@@ -24,3 +37,7 @@ if [[ ! -s "$_autoload/plug.vim" ]]; then
   _url="https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
   curl --fail --silent "$_url" > "$_autoload/plug.vim"
 fi
+
+symlink_files
+
+vim +PlugInstall +qall
